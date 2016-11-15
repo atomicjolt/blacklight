efc3f64bd740afef2f1bb98ce306eab74199643d
KamiJean <kamijeanwilson@gmail.com>
2016-11-15 14:56:40 -0700

    Fixing specs on the course

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -21,20 +21,42 @@ describe Blacklight do
 			end
 		end
 
+		describe "#create_canvas_cc_course" do
+			it "should return with a course" do
+				canvas_course = @course.instance_variable_get :@canvas_course
+				assert_equal (canvas_course.is_a? Object), true
+			end
+		end
+
 		describe "#open_file" do
 			it "should return with an error" do
 				err = assert_raises(Exception) { @course.open_file("") }
 				assert_match /Couldn't find file/, err.message
 			end
 		end
-	end
 
-	describe "#create_canvas_cc_course" do
-		it "should return with a course" do
-			course = Blacklight.create_canvas_cc_course
-			is_object = course.is_a? Object
-			assert_equal is_object, true
+		describe "#set_course_values" do
+			it "should set the canvas course values" do
+				name = "identification"
+				value = 'RandomID23joirjeowijafoi'
+				@course.set_course_values(name, value)
+				canvas_course = @course.instance_variable_get :@canvas_course
+				assert_equal canvas_course.send(name.to_sym), value
+			end
+
+			it "should not override previous settings" do
+				name = "identification"
+				value = 'RandomID23joirjeowijafoi'
+				@course.set_course_values(name, value)
+				name2 = "title"
+				value2 = 'The Randomest Course'
+				@course.set_course_values(name2, value2)
+				canvas_course = @course.instance_variable_get :@canvas_course
+				assert_equal canvas_course.send(name.to_sym), value
+				assert_equal canvas_course.send(name2.to_sym), value2
+			end
 		end
+
 	end
 
 end
