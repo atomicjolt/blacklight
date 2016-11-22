2eeef6ef33dcbd6d952e9bb59ec6b09c596c3c5c
Nick <nick.benoit14@gmail.com>
2016-11-22 11:57:36 -0700

    add content spec

---
+++ spec/models/content_spec.rb
@@ -0,0 +1,23 @@
+require "minitest/autorun"
+require "blacklight"
+require "pry"
+
+require_relative "../helpers.rb"
+require_relative "../../lib/blacklight/models/content"
+
+describe "Content" do
+  it "should iterate_xml" do
+    xml = get_fixture_xml "content.xml"
+    content = Blacklight::Content.new
+    content.iterate_xml(xml)
+
+    content_title = xml.xpath("/CONTENT/TITLE/@value").first.text
+    content_body = xml.xpath("/CONTENT/BODY/TEXT").first.text
+    content_id = xml.xpath("/CONTENT/@id").first.text
+
+    assert_equal(content.id, content_id)
+    assert_equal(content.title, content_title)
+    assert_equal(content.body, content_body)
+    assert_equal(content.files, nil)
+  end
+end
