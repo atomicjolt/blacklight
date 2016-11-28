c3bfd53c15e1023df4908a1a863ca1e41fde5d33
Nick <nick.benoit14@gmail.com>
2016-11-28 09:32:08 -0700

    add file canvas conversion

--- lib/blacklight/models/file.rb
+++ lib/blacklight/models/file.rb
@@ -3,13 +3,6 @@ module Blacklight
   class BlacklightFile
     attr_accessor(:id, :location, :name)
 
-    # def iterate_xml(xml)
-      # @title = xml.xpath("/CONTENT/TITLE/@value").first.text
-      # @body = xml.xpath("/CONTENT/BODY/TEXT").first.text
-      # @id = xml.xpath("/CONTENT/@id").first.text
-      # @files = []
-    # end
-
     def initialize(path)
       name = File.basename(path)
       id = name.scan(/__(xid-[0-9]+)/).first
@@ -20,7 +13,15 @@ module Blacklight
       @id = id.first
     end
 
-    def canvas_conversion
+    def canvas_conversion(course)
+      file = CanvasCc::CanvasCC::Models::CanvasFile.new
+      file.identifier = @id
+      file.file_location = @location
+      file.file_path = @name
+      file.hidden = false
+
+      course.files << file
+      course
     end
   end
 end
--- spec/models/file_spec.rb
+++ spec/models/file_spec.rb
@@ -13,4 +13,15 @@ describe "Content" do
     assert_equal(file.name, File.basename(path))
     assert_equal(file.location, path)
   end
+
+  it "should convert to canvas file " do
+    path = "fake/path/to/file__xid-1234.txt"
+    file = Blacklight::BlacklightFile.new(path)
+    course = CanvasCc::CanvasCC::Models::Course.new
+
+    result = file.canvas_conversion(course)
+
+    assert_equal(result.files.size, 1)
+    assert_equal(result.files.first.identifier, "xid-1234")
+  end
 end
