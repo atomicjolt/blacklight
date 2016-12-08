86fe27640f09b25cad55eae4a3d398262b59b132
Nick Benoit <nick.benoit14@gmail.com>
2016-12-07 18:40:42 -0700

    add get_entries

--- lib/blacklight/models/scorm_package.rb
+++ lib/blacklight/models/scorm_package.rb
@@ -1,7 +1,22 @@
 module Blacklight
   class ScormPackage
-    def initialize(manifest)
-      byebug
+    attr_accessor(:entries, :manifest)
+
+    def initialize(zip_file, manifest)
+      @manifest = manifest
+      @entries = ScormPackage.get_entries zip_file, manifest
+    end
+
+    def self.get_entries(zip_file, manifest)
+      zip_file.entries.select do |e|
+        File.dirname(e.name).include? File.dirname(manifest.name)
+      end
+    end
+
+    def to_zip
+      # TODO use get_output_stream, and then both put_next_entry, and write to write
+      # new zip
+      # http://stackoverflow.com/questions/23957178/how-can-i-create-a-zip-file-without-temporary-files-in-ruby
     end
 
     def canvas_conversion
