18b16e9f892d7a0d3f03a239746e3f0a11e15c3f
James Carbine <jamescarbine@gmail.com>
2016-12-12 13:14:08 -0700

    Rename methods

--- lib/blacklight.rb
+++ lib/blacklight.rb
@@ -13,23 +13,23 @@ require_relative "./blacklight/exceptions"
 module Blacklight
   def self.parse(zip_path, imscc_path)
     Zip::File.open(zip_path) do |file|
-      manifest = open_file(file, "imsmanifest.xml")
+      manifest = read_file(file, "imsmanifest.xml")
 
       resources = Blacklight.parse_manifest(file, manifest)
       resources.concat(Blacklight.iterate_files(file))
 
       course = create_canvas_course(resources, zip_path)
-      output_to_dir(course, imscc_path)
+      build_file(course, imscc_path)
     end
   end
 
-  def self.open_file(zip_file, file_name)
+  def self.read_file(zip_file, file_name)
     zip_file.find_entry(file_name).get_input_stream.read
   rescue NoMethodError
     raise Exceptions::MissingFileError
   end
 
-  def self.output_to_dir(course, imscc_path)
+  def self.build_file(course, imscc_path)
     folder = imscc_path.split("/").first
     file = CanvasCc::CanvasCC::CartridgeCreator.new(course).create(folder)
     File.rename(file, imscc_path)
--- spec/blacklight_spec.rb
+++ spec/blacklight_spec.rb
@@ -5,10 +5,10 @@ require "pry"
 include Blacklight
 
 describe Blacklight do
-  describe "open_file" do
+  describe "read_file" do
     it "should return with an error" do
       file_path = File.expand_path("../fixtures/", __FILE__) + "/test.zip"
-      err = assert_raises(Exception) { Blacklight.open_file(file_path, "") }
+      err = assert_raises(Exception) { Blacklight.read_file(file_path, "") }
       assert_match /Couldn't find file/, err.message
     end
   end
