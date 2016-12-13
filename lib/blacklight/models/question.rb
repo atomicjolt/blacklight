bd5713faef8d6962671be1063293cc5f5606fe50
Conrad Beach <conradbeach@gmail.com>
2016-12-13 08:46:58 -0700

    Begins improving formula/calculated question handling.

--- lib/blacklight/models/question.rb
+++ lib/blacklight/models/question.rb
@@ -10,7 +10,7 @@ module Blacklight
       "Fill in the Blank Plus" => "fill_in_multiple_blanks_question",
       "File Upload" => "file_upload_question",
       "Essay" => "essay_question",
-      "Calculated" => "short_answer_question",
+      "Calculated" => "formula_question",
       "Jumbled Sentence" => "multiple_dropdowns_question",
       "Either/Or" => "multiple_choice_question",
       "Hot Spot" => "text_only_question",
--- lib/blacklight/models/questions/calculated.rb
+++ lib/blacklight/models/questions/calculated.rb
@@ -2,11 +2,50 @@ module Blacklight
   class Calculated < Question
     def iterate_xml(data)
       super
+      calculated_node = data.at("itemproc_extension > calculated")
+      formula = CGI.unescapeHTML(calculated_node.at("forumla").text)
+      answer = Answer.new(forumla)
+      @answers.push(answer)
+      answer_tolerance = calculated_node.at("answer_tolerance").text
+      decimal_places = 0
+
+      @vars = parse_vars(calculated_node.at("vars"))
+      var_sets = parse_var_sets(calculated_node.at("var_sets"))
+
       answer_text = CGI.unescapeHTML(data.at("formula").text)
       answer = Answer.new(answer_text)
       answer.fraction = 1
       @answers.push(answer)
       self
+    end
+
+    def parse_vars(parent_node)
+      parent_node.search("var").map do |var|
+        scale = var.attributes["scale"]
+        min = var.at("min").text
+        max = var.at("max").text
+        options = ":#{min}:#{max}:#{scale}"
+
+        {
+          name: var.attributes["name"],
+          options: options,
+        }
+      end
+    end
+
+    def parse_var_sets(parent_node)
+      parent_node.search("var_set").map do |var_set|
+        var = var_set.at("var")
+
+        {
+          ident: var_set.attributes["ident"],
+          answer: var_set.at("answer").text,
+          var: {
+            name: var.attributes["name"],
+            text: var.text,
+          },
+        }
+      end
     end
   end
 end
