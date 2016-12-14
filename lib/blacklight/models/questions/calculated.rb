252b460a6bd7631f22fcf5d45edaff0b2d876106
Conrad Beach <conradbeach@gmail.com>
2016-12-14 14:30:24 -0700

    Formula question (#12)
    * Begins improving formula/calculated question handling.
    
    * Further formula question implementation.
    
    * Complete formula question implementation.
    
    * Refactor parse_var_sets.
    
    * Add tests.
    
    * Refactor calculated question.

--- lib/blacklight/models/questions/calculated.rb
+++ lib/blacklight/models/questions/calculated.rb
@@ -1,12 +1,72 @@
 module Blacklight
   class Calculated < Question
+    attr_reader :dataset_definitions, :var_sets
+
+    def initialize
+      @dataset_definitions = []
+      @var_sets = []
+      @correct_answer_length = 0
+      @correct_answer_format = 0
+      @tolerance = 0
+      super
+    end
+
+    def canvas_conversion(*)
+      @question.dataset_definitions = @dataset_definitions
+      @question.var_sets = @var_sets
+      @question.correct_answer_length = @correct_answer_length
+      @question.correct_answer_format = @correct_answer_format
+      @question.tolerance = @tolerance
+      super
+    end
+
     def iterate_xml(data)
       super
-      answer_text = CGI.unescapeHTML(data.at("formula").text)
-      answer = Answer.new(answer_text)
-      answer.fraction = 1
+      calculated_node = data.at("itemproc_extension > calculated")
+      math_ml = CGI.unescapeHTML(calculated_node.at("formula").text)
+      formula = Nokogiri::HTML(math_ml).text
+
+      answer = Answer.new(formula)
       @answers.push(answer)
+
+      @tolerance = calculated_node.at("answer_tolerance").text
+      # canvas_cc only uses the correct_answer_length if the
+      # correct_answer_format is 1. It is not known what 1 represents.
+      @correct_answer_format = 1
+      @correct_answer_length = calculated_node.at("answer_scale").text
+
+      @dataset_definitions = _parse_vars(calculated_node.at("vars"))
+      @var_sets = _parse_var_sets(calculated_node.at("var_sets"))
+
       self
+    end
+
+    def _parse_vars(parent_node)
+      parent_node.search("var").map do |var|
+        scale = var.attributes["scale"]
+        min = var.at("min").text
+        max = var.at("max").text
+        options = ":#{min}:#{max}:#{scale}"
+
+        {
+          name: var.attributes["name"].value,
+          options: options,
+        }
+      end
+    end
+
+    def _parse_var_sets(parent_node)
+      parent_node.search("var_set").map do |var_set|
+        vars = var_set.search("var").each_with_object({}) do |var, hash|
+          hash[var.attributes["name"].text] = var.text
+        end
+
+        {
+          ident: var_set.attributes["ident"].text,
+          answer: var_set.at("answer").text,
+          vars: vars,
+        }
+      end
     end
   end
 end
--- spec/models/questions/calculated_spec.rb
+++ spec/models/questions/calculated_spec.rb
@@ -17,32 +17,43 @@ describe Blacklight do
     end
 
     describe "iterate_xml" do
-      it "should iterate through xml and have one answer" do
+      before do
         xml = get_fixture_xml "calculated.xml"
         @calculated = @calculated.iterate_xml(xml.children.first)
+      end
 
-        answers = @calculated.instance_variable_get :@answers
+      it "should iterate through xml and have one answer" do
+        answers = @calculated.answers
         assert_equal answers.count, 1
       end
 
-      it "should iterate through xml and right content to answers" do
-        xml = get_fixture_xml "calculated.xml"
-        @calculated = @calculated.iterate_xml(xml.children.first)
+      it "should iterate through xml and write content to answers" do
+        answers = @calculated.answers
 
-        answers = @calculated.instance_variable_get :@answers
-
-        assert_includes (answers.first.instance_variable_get :@answer_text),
-                        "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">"
-        assert_includes (answers.first.instance_variable_get :@answer_text),
-                        "<mroot><msup><mn>24</mn><mrow/></msup><mrow/></mroot>"
+        assert_includes answers.first.answer_text, "24"
       end
 
-      it "should iterate and contain the right fraction" do
-        xml = get_fixture_xml "calculated.xml"
-        @calculated = @calculated.iterate_xml(xml.children.first)
+      it "should populate dataset_definitions" do
+        assert_equal @calculated.dataset_definitions.length, 2
 
-        answers = @calculated.instance_variable_get :@answers
-        assert_equal (answers.first.instance_variable_get :@fraction), 1
+        definitions = @calculated.dataset_definitions.sort_by do |definition|
+          definition[:name]
+        end
+
+        assert_equal definitions.first[:name], "x"
+        assert_equal definitions.first[:options], ":10.0:100.0:2"
+      end
+
+      it "should populate var_sets" do
+        assert_equal @calculated.var_sets.length, 2
+
+        sets = @calculated.var_sets.sort_by { |set| set[:ident] }
+
+        assert_equal sets.first[:ident], "a"
+        assert_equal sets.first[:answer], "183.1"
+
+        correct_vars = { "x" => "95.57", "y" => "87.53" }
+        assert_equal sets.first[:vars], correct_vars
       end
     end
   end
