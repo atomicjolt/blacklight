6c4a85ab0649ab996c8ea27c544eb43da12fa576
Conrad Beach <conradbeach@gmail.com>
2016-12-16 15:37:49 -0700

    Remove `fix_images` from `Question`.

--- lib/blacklight/models/question.rb
+++ lib/blacklight/models/question.rb
@@ -99,26 +99,6 @@ module Blacklight
       assessment
     end
 
-    def fix_images(contents, resources)
-      if contents && !contents.empty?
-        node_html = Nokogiri::HTML.fragment(contents)
-
-        node_html.search("img").each do |element|
-          original_src = element["src"]
-          xid = original_src.split("/").last
-          file_resource = resources.detect_xid(xid)
-
-          if file_resource
-            element["src"] = "$IMS-CC-FILEBASE$/#{file_resource.name}"
-          end
-        end
-
-        node_html.to_s
-      else
-        contents
-      end
-    end
-
     def get_fraction(answer_text)
       if @correct_answers && answer_text == @correct_answers["name"]
         @correct_answers["fraction"].to_f
