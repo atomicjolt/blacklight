971fd62f8aaf0136f8ba61723fae61e653e82770
Conrad Beach <conradbeach@gmail.com>
2016-12-19 16:59:44 -0700

    Add to and refactor `BlacklightFile` specs.

--- spec/models/file_spec.rb
+++ spec/models/file_spec.rb
@@ -6,25 +6,29 @@ require_relative "../mocks/mockzip"
 require_relative "../../lib/blacklight/models/file"
 
 describe "BlacklightFile" do
-  it "should iterate_xml" do
-    path = "fake/path/to/file__xid-1234.txt"
-    entry = MockZip::MockEntry.new(path)
-    file = Blacklight::BlacklightFile.new(entry)
+  before do
+    @path = "fake/path/to/file__xid-1234_1.txt"
+    entry = MockZip::MockEntry.new(@path)
+    @file = Blacklight::BlacklightFile.new(entry)
+  end
 
-    assert_equal(file.id, "file__xid-1234.txt")
-    assert_equal(file.name, File.basename(path))
-    assert_includes(file.location, path)
+  it "should iterate_xml" do
+    assert_equal(@file.id, "file__xid-1234_1.txt")
+    assert_equal(@file.name, "file.txt")
+    assert_includes(@file.location, @path)
   end
 
   it "should convert to canvas file " do
-    path = "fake/path/to/file__xid-1234.txt"
-    entry = MockZip::MockEntry.new(path)
-    file = Blacklight::BlacklightFile.new(entry)
     course = CanvasCc::CanvasCC::Models::Course.new
 
-    result = file.canvas_conversion(course)
+    result = @file.canvas_conversion(course)
 
     assert_equal(result.files.size, 1)
-    assert_equal(result.files.first.identifier, "file__xid-1234.txt")
+    assert_equal(result.files.first.identifier, "file__xid-1234_1.txt")
+  end
+
+  it "matches_xid? method returns the correct results" do
+    assert_equal(@file.matches_xid?("xid-1234_1"), true)
+    assert_equal(@file.matches_xid?("xid-5678_1"), false)
   end
 end
