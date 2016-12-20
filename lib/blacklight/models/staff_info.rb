15c124af91cd7b733d975956ebf276e211c9680f
Nick Benoit <nick.benoit14@gmail.com>
2016-12-20 14:15:32 -0700

    adds blacklight module

--- lib/blacklight/models/staff_info.rb
+++ lib/blacklight/models/staff_info.rb
@@ -1,49 +1,57 @@
-class StaffInfo
-  attr_accessor(
-    :id,
-    :title,
-    :bio,
-    :name,
-    :email,
-    :phone,
-    :office_hours,
-    :office_address,
-    :home_page,
-    :image,
-  )
 
-  def parse_name(contact)
-    parts = [
-      contact.xpath("./NAME/FORMALTITLE/@value").text,
-      contact.xpath("./NAME/GIVEN/@value").text,
-      contact.xpath("./NAME/FAMILY/@value").text,
-    ]
+module Blacklight
+  class StaffInfo
+    attr_accessor(
+      :id,
+      :title,
+      :bio,
+      :name,
+      :email,
+      :phone,
+      :office_hours,
+      :office_address,
+      :home_page,
+      :image,
+    )
 
-    resp = ""
-    parts.each do |part|
-      resp << " " unless resp.empty?
-      resp << part unless part.empty?
+    def parse_name(contact)
+      parts = [
+        contact.xpath("./NAME/FORMALTITLE/@value").text,
+        contact.xpath("./NAME/GIVEN/@value").text,
+        contact.xpath("./NAME/FAMILY/@value").text,
+      ]
+
+      resp = ""
+      parts.each do |part|
+        resp << " " unless resp.empty?
+        resp << part unless part.empty?
+      end
+      resp
     end
-    resp
-  end
 
-  def iterate_xml(xml)
-    contact = xml.xpath("//CONTACT")
-    @id = xml.xpath("//STAFFINFO/@id").text
-    @title = xml.xpath("//STAFFINFO/TITLE/@value").text
-    @bio = xml.xpath("//BIOGRAPHY/TEXT").text
-    @name = parse_name(contact)
-    @email = xml.xpath("//CONTACT/EMAIL/@value").text
-    @phone = xml.xpath("//CONTACT/PHONE/@value").text
-    @office_hours = xml.xpath("//OFFICE/HOURS/@value").text
-    @office_address = xml.xpath("//OFFICE/ADDRESS/@value").text
-    @home_page = xml.xpath("//HOMEPAGE/@value").text
-    @image = xml.xpath("//IMAGE/@value").text
+    def iterate_xml(xml)
+      contact = xml.xpath("//CONTACT")
+      @id = xml.xpath("//STAFFINFO/@id").text
+      @title = xml.xpath("//STAFFINFO/TITLE/@value").text
+      @bio = xml.xpath("//BIOGRAPHY/TEXT").text
+      @name = parse_name(contact)
+      @email = xml.xpath("//CONTACT/EMAIL/@value").text
+      @phone = xml.xpath("//CONTACT/PHONE/@value").text
+      @office_hours = xml.xpath("//OFFICE/HOURS/@value").text
+      @office_address = xml.xpath("//OFFICE/ADDRESS/@value").text
+      @home_page = xml.xpath("//HOMEPAGE/@value").text
+      @image = xml.xpath("//IMAGE/@value").text
 
-    self
-  end
+      self
+    end
 
-  def canvas_conversion(course)
-    course
+    def canvas_conversion(course)
+      page = CanvasCc::CanvasCC::Models::Page.new
+      # page.body = @body
+      # page.identifier = @id
+      # page.page_name = @title
+
+      course
+    end
   end
 end
