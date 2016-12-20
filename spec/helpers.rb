d9b6f7693c207042c9349974e8b0795b323cacc6
James Carbine <jamescarbine@gmail.com>
2016-12-20 10:45:39 -0700

    Fix scorm package spec to close fixtures

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -4,6 +4,18 @@ def get_fixture(name)
   end
 end
 
+def get_zip_fixture(name)
+  Zip::File.open("#{File.dirname(__FILE__)}/fixtures/#{name}") do |file|
+    yield file
+  end
+end
+
+def get_zip_manifest(name)
+  get_zip_fixture(name) do |file|
+    return file, get_manifest_entry(file)
+  end
+end
+
 def get_fixture_xml(name)
   get_fixture(name) do |file|
     Nokogiri::XML(file)
--- spec/models/scorm_package_spec.rb
+++ spec/models/scorm_package_spec.rb
@@ -12,8 +12,7 @@ end
 
 describe "ScormPackage" do
   it "should find all entries in same directory as manifest" do
-    zip = Zip::File.new("spec/fixtures/scorm_package.zip")
-    manifest = get_manifest_entry zip
+    zip, manifest = get_zip_manifest("scorm_package.zip")
 
     result = Blacklight::ScormPackage.get_entries(zip, manifest)
     assert_equal(result.size, 2)
@@ -24,13 +23,14 @@ describe "ScormPackage" do
   end
 
   it "should convert to zip file" do
-    zip = Zip::File.new("spec/fixtures/scorm_package.zip")
-    package = Blacklight::ScormPackage.new(zip, get_manifest_entry(zip))
+    zip, manifest = get_zip_manifest("scorm_package.zip")
+    package = Blacklight::ScormPackage.new(zip, manifest)
     EXPORT_NAME = "zip_export.zip".freeze
     begin
       result_location = package.write_zip(EXPORT_NAME)
-      result_zip = Zip::File.new(result_location)
-      result_manifest = get_manifest_entry(result_zip)
+      result_manifest = Zip::File.open(result_location) do |file|
+        get_manifest_entry(file)
+      end
 
       assert_equal(
         result_manifest.get_input_stream.read.include?("ADL SCORM"),
