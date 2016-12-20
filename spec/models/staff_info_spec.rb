8c4655e911082cd712c46dd679d9bb8a936d84a3
Nick Benoit <nick.benoit14@gmail.com>
2016-12-20 13:05:02 -0700

    adds staff_info spec

---
+++ spec/models/staff_info_spec.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require "blacklight"
+require "pry"
+
+require_relative "../mocks/mockzip"
+require_relative "../../lib/blacklight/models/staff_info"
+
+describe "StaffInfo" do
+
+  it "should parse xml" do
+    # pull out biography
+    # pull out contact
+  end
+
+  it "should convert to canvas page" do
+  end
+
+end
