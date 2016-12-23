0fb1df8b279e23d1504e9502a66b1ec3ad599df4
James Carbine <jamescarbine@gmail.com>
2016-12-23 14:52:35 -0700

    Same for fill in the blank plus

--- lib/blacklight/models/questions/fill_in_blank_plus.rb
+++ lib/blacklight/models/questions/fill_in_blank_plus.rb
@@ -3,12 +3,15 @@ module Blacklight
     def iterate_xml(data)
       super
       conditionvar = data.at("resprocessing").at("conditionvar")
-      conditionvar.at("and").children.each do |or_child|
-        or_child.children.each do |varequal|
-          answer = Answer.new(varequal.text)
-          answer.resp_ident = varequal.attributes["respident"].value
-          answer.fraction = @max_score
-          @answers.push(answer)
+      # not all fill in the blank questions have answers(ie: surveys)
+      if conditionvar
+        conditionvar.at("and").children.each do |or_child|
+          or_child.children.each do |varequal|
+            answer = Answer.new(varequal.text)
+            answer.resp_ident = varequal.attributes["respident"].value
+            answer.fraction = @max_score
+            @answers.push(answer)
+          end
         end
       end
       self
