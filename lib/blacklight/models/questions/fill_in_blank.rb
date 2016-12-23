8a7c88f546e8ca7020e70b88a94147df95b42acc
James Carbine <jamescarbine@gmail.com>
2016-12-23 13:26:18 -0700

    Make sure an answer is found for fill in the blank

--- lib/blacklight/models/questions/fill_in_blank.rb
+++ lib/blacklight/models/questions/fill_in_blank.rb
@@ -3,9 +3,12 @@ module Blacklight
     def iterate_xml(data)
       super
       conditionvar = data.at("resprocessing").at("conditionvar")
-      answer = Answer.new(conditionvar.at("varequal").text)
-      answer.fraction = @max_score
-      @answers.push(answer)
+      # not all fill in the blank questions have answers(ie: surveys)
+      if conditionvar
+        answer = Answer.new(conditionvar.at("varequal").text)
+        answer.fraction = @max_score
+        @answers.push(answer)
+      end
       self
     end
   end
