2fc6a3c30fdc30bb37aaae18b173311c0c859580
KamiJean <kamijeanwilson@gmail.com>
2017-01-03 18:07:26 -0700

    Adding the missed external url set module function

--- lib/senkyoshi/models/external_url.rb
+++ lib/senkyoshi/models/external_url.rb
@@ -5,5 +5,10 @@ module Senkyoshi
     def canvas_conversion(course, _resource)
       create_module(course)
     end
+
+    def set_module
+      module_item = ModuleItem.new(@title, @module_type, @id, @url)
+      module_item.canvas_conversion
+    end
   end
 end
