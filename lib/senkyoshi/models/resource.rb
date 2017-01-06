b35495df6331492409e953f245927af0139c7aa5
Nick Benoit <nick.benoit14@gmail.com>
2017-01-06 10:58:06 -0700

    remove IMPORTED_FILES_DIRNAME from resource

--- lib/senkyoshi/models/resource.rb
+++ lib/senkyoshi/models/resource.rb
@@ -23,10 +23,8 @@ module Senkyoshi
         if original_src
           xid = original_src.split("/").last
           file_resource = resources.detect_xid(xid)
-
           if file_resource
-            path = file_resource.path
-            element[attr] = "#{BASE}/#{IMPORTED_FILES_DIRNAME}/#{path}"
+            element[attr] = "#{BASE}/#{file_resource.path}"
           end
         end
       end
