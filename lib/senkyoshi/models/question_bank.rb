a6e69db290109ac5a63a1b2168d12820284f70e9
KamiJean <kamijeanwilson@gmail.com>
2017-01-06 22:03:57 -0700

    Adding description to the reason for the sub

--- lib/senkyoshi/models/question_bank.rb
+++ lib/senkyoshi/models/question_bank.rb
@@ -28,6 +28,8 @@ module Senkyoshi
       question_bank.questions = []
       questions.each do |item|
         question = item.canvas_conversion(question_bank, resources)
+        # This is to remove the random extra <p>.</p> included in the
+        # description that is just randomly there
         tag = "<p><span size=\"2\" style=\"font-size: small;\">.</span></p>"
         question.material.gsub!(tag, "")
         question.material.gsub!("<p>.</p>", "")
