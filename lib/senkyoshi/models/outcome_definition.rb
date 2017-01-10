e13cf3ee9a3f428b4bf27ad3ee689b9db0e55624
Nick Benoit <nick.benoit14@gmail.com>
2017-01-10 12:49:21 -0700

    adds outcome_definitino model

---
+++ lib/senkyoshi/models/outcome_definition.rb
@@ -0,0 +1,24 @@
+require "byebug"
+module Senkyoshi
+  class OutcomeDefinition
+    def initialize(id, content_id)
+      @id = id
+      @content_id = content_id
+    end
+
+    def canvas_conversion(course, _)
+      # Create an assignment
+      course
+    end
+
+    def self.from_xml(xml)
+      content_id = xml.xpath("./CONTENTID/@value").text
+      id = xml.xpath("./@id").text
+      OutcomeDefinition.new(id, content_id)
+    end
+
+    def self.should_create_assignment?(xml)
+      xml.xpath("./OUTCOMEDEFINITION/CONTENTID/@value").first.text
+    end
+  end
+end
