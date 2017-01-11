db87a8b3b910a7dd41783453046470426b92e398
Nick Benoit <nick.benoit14@gmail.com>
2017-01-10 17:16:43 -0700

    add outcome definition members

--- lib/senkyoshi/models/outcome_definition.rb
+++ lib/senkyoshi/models/outcome_definition.rb
@@ -1,20 +1,27 @@
 require "byebug"
 module Senkyoshi
   class OutcomeDefinition
-    def initialize(id, content_id)
+    attr_reader(:id, :content_id, :title, :points_possible)
+
+    def initialize(id, content_id, title, points_possible)
       @id = id
       @content_id = content_id
+      @title = title
+      @points_possible = points_possible
     end
 
     def canvas_conversion(course, _)
       # Create an assignment
+      assignment = CanvasCc::CanvasCC::Models::Assignment.new
+
+      course.assignments << assignment
       course
     end
 
     def self.from_xml(xml)
       content_id = xml.xpath("./CONTENTID/@value").text
       id = xml.xpath("./@id").text
-      OutcomeDefinition.new(id, content_id)
+      OutcomeDefinition.new(id, content_id, nil, nil)
     end
   end
 end
