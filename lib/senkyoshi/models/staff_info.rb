af6ec73c2e27eda39b96fd9dda3bc0b5255286f2
James Carbine <JamesCarbine@gmail.com>
2017-01-11 13:12:58 -0700

    Fix generating contact pages (#45)
    * Fix generating contact pages
    Remove class variables.
    Use only one instance of StaffInfo.
    
    * DRY up adding contact details

--- lib/senkyoshi/models/staff_info.rb
+++ lib/senkyoshi/models/staff_info.rb
@@ -1,28 +1,16 @@
 require "senkyoshi/models/resource"
+require "active_support/core_ext/string"
 
 module Senkyoshi
   class StaffInfo < Resource
     attr_reader(
       :id,
       :title,
-      :bio,
-      :name,
-      :email,
-      :phone,
-      :office_hours,
-      :office_address,
-      :home_page,
-      :image,
+      :entries,
     )
 
-    def self.reset_entries
-      @@entries = []
-      @@page_created = false
-    end
-
     def initialize
-      @@entries ||= []
-      @@page_created = false
+      @entries = []
     end
 
     def parse_name(contact)
@@ -42,51 +30,58 @@ module Senkyoshi
 
     def iterate_xml(xml, _pre_data)
       contact = xml.xpath("//CONTACT")
-      @id = xml.xpath("//STAFFINFO/@id").text
-      @title = xml.xpath("//STAFFINFO/TITLE/@value").text
-      @bio = xml.xpath("//BIOGRAPHY/TEXT").text
-      @name = parse_name(contact)
-      @email = xml.xpath("//CONTACT/EMAIL/@value").text
-      @phone = xml.xpath("//CONTACT/PHONE/@value").text
-      @office_hours = xml.xpath("//OFFICE/HOURS/@value").text
-      @office_address = xml.xpath("//OFFICE/ADDRESS/@value").text
-      @home_page = xml.xpath("//HOMEPAGE/@value").text
-      @image = xml.xpath("//IMAGE/@value").text
+      @id ||= xml.xpath("//STAFFINFO/@id").text || Senkyoshi.create_random_hex
+      @title ||= xml.xpath("//STAFFINFO/TITLE/@value").text
+      bio = xml.xpath("//BIOGRAPHY/TEXT").text
+      name = parse_name(contact)
+      email = xml.xpath("//CONTACT/EMAIL/@value").text
+      phone = xml.xpath("//CONTACT/PHONE/@value").text
+      office_hours = xml.xpath("//OFFICE/HOURS/@value").text
+      office_address = xml.xpath("//OFFICE/ADDRESS/@value").text
+      home_page = xml.xpath("//HOMEPAGE/@value").text
+      image = xml.xpath("//IMAGE/@value").text
 
-      @@entries << construct_body
+      @entries << construct_body(
+        bio: bio,
+        name: name,
+        email: email,
+        phone: phone,
+        office_hours: office_hours,
+        office_address: office_address,
+        home_page: home_page,
+        image: image,
+      )
 
       self
     end
 
-    def add_to_body(str, var)
-      @body << str if var && !var.empty?
+    def append_str(body, str, var)
+      body << str if var && !var.empty?
     end
 
-    def construct_body
-      @body = "<div>"
-      add_to_body "<img src=#{@image}/>", @image
-      add_to_body "<h3>#{@name}</h3>", @name
-      add_to_body "<p>#{@bio}</p>", @bio
+    def humanize(symbol)
+      symbol.to_s.humanize.titleize
+    end
 
-      @body << "<ul>"
-      add_to_body "<li>Email: #{@email}</li>", @email
-      add_to_body "<li>Phone: #{@phone}</li>", @phone
-      add_to_body "<li>Office Hours: #{@office_hours}</li>", @office_hours
-      add_to_body "<li>Office Address: #{@office_address}</li>", @office_address
-      add_to_body "<li>Home Page: #{@home_page}</li>", @home_page
-      @body << "</ul></div>"
+    def construct_body(opts)
+      body = "<div>"
+      append_str body, "<img src=#{opts[:image]}/>", opts[:image]
+      append_str body, "<h3>#{opts[:name]}</h3>", opts[:name]
+      append_str body, "<p>#{opts[:bio]}</p>", opts[:bio]
+
+      body << "<ul>"
+      [:email, :phone, :office_hours, :office_address, :home_page].each do |key|
+        append_str body, "<li>#{humanize(key)}: #{opts[key]}</li>", opts[key]
+      end
+      body << "</ul></div>"
+      body
     end
 
     def canvas_conversion(course, resources)
-      # We want to create only a single "Contact" page, so once we already have
-      # a StaffInfo resource, we won't want another one
-      return course if @@page_created
-
       page = CanvasCc::CanvasCC::Models::Page.new
-      page.body = fix_html(@@entries.join(" "), resources)
+      page.body = fix_html(@entries.join(" "), resources)
       page.identifier = @id
       page.page_name = @title.empty? ? "Contact" : @title
-      @@page_created = true
 
       course.pages << page
       course
