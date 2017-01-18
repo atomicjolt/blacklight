306b1e95ae7772571083ddbb9f695daaa8d73278
Conrad Beach <conradbeach@gmail.com>
2017-01-18 13:15:53 -0700

    Fix broken links to directories.

--- lib/senkyoshi/models/resource.rb
+++ lib/senkyoshi/models/resource.rb
@@ -19,15 +19,49 @@ module Senkyoshi
       false
     end
 
+    def strip_xid(name)
+      name.gsub(/__xid-[0-9]+_[0-9]+/, "")
+    end
+
+    def _matches_directory_xid?(xid, directory)
+      dir_xid = directory[/xid-[0-9]+_[0-9]+\z/]
+      xid == dir_xid
+    end
+
+    def _find_directories(resources)
+      resources.resources.map do |resource|
+        if resource.respond_to?(:location)
+          File.dirname(resource.location)[/csfiles.*/]
+        end
+      end.uniq.compact
+    end
+
+    def _fix_path(original_src, resources, dir_names)
+      xid = original_src.split("/").last
+      file_resource = resources.detect_xid(xid)
+
+      if file_resource
+        "#{FILE_BASE}/#{file_resource.path}"
+      else
+        matching_dir = dir_names.detect do |dir|
+          _matches_directory_xid?(xid, dir)
+        end
+
+        if matching_dir
+          "#{DIR_BASE}/#{strip_xid(matching_dir)}"
+        end
+      end
+    end
+
     def _search_and_replace(resources, node_html, tag, attr)
+      dir_names = _find_directories(resources)
+
       node_html.search(tag).each do |element|
         original_src = element[attr]
+
         if original_src
-          xid = original_src.split("/").last
-          file_resource = resources.detect_xid(xid)
-          if file_resource
-            element[attr] = "#{FILE_BASE}/#{file_resource.path}"
-          end
+          path = _fix_path(original_src, resources, dir_names)
+          element[attr] = path if path
         end
       end
     end
