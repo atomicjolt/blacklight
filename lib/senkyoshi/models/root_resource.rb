badd95448c1634cfc1e4793d49f80ddfd7010818
Nick Benoit <nick.benoit14@gmail.com>
2017-01-19 16:56:51 -0700

    adds root resource

---
+++ lib/senkyoshi/models/root_resource.rb
@@ -0,0 +1,20 @@
+require "senkyoshi/models/resource"
+
+module Senkyoshi
+  class RootResource < Resource
+    attr_reader(:id)
+
+    def initialize(id = nil)
+      @id = id
+    end
+
+    def self.from(xml = nil, pre_data = nil, _resource_xids = nil)
+      resource = new(pre_data[:file_name])
+      resource.iterate_xml(xml, pre_data)
+    end
+
+    def iterate_xml(_xml, _pre_data)
+      self
+    end
+  end
+end
