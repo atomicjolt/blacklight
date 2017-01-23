0b0f11f60f1d3c935c816439fdaba7af27163731
Nick Benoit <nick.benoit14@gmail.com>
2017-01-23 11:35:54 -0700

    adds Module.find_module_from_item_id

--- spec/models/module_spec.rb
+++ spec/models/module_spec.rb
@@ -11,6 +11,31 @@ describe "Module" do
     @identifier = Senkyoshi.create_random_hex
     @module = Senkyoshi::Module.new(@title, @identifier)
     @url = "fake/url"
+
+    @module_ids = ["1", "2"]
+
+    @canvas_modules = @module_ids.map do |id|
+      CanvasCc::CanvasCC::Models::CanvasModule.new.tap do |mod|
+        mod.identifier = id
+      end
+    end
+
+    @page_ids = ["res001", "res002"]
+    @canvas_pages = @page_ids.map do |id|
+      CanvasCc::CanvasCC::Models::Page.new.tap do |page|
+        page.identifier = id
+      end
+    end
+
+    @canvas_module_items = @page_ids.map do |id|
+      CanvasCc::CanvasCC::Models::ModuleItem.new.tap do |item|
+        item.identifierref = id
+      end
+    end
+
+    @canvas_modules.each_with_index do |mod, index|
+      mod.module_items << @canvas_module_items[index]
+    end
   end
 
   describe "initialize" do
@@ -18,6 +43,16 @@ describe "Module" do
       assert_equal (@module.is_a? Object), true
       assert_equal (@module.instance_variable_get :@title), @title
       assert_equal (@module.instance_variable_get :@identifier), @identifier
+    end
+  end
+
+  describe "find_module_from_item_id" do
+    it "should return module that module item belongs to" do
+      result = Senkyoshi::Module.find_module_from_item_id(
+        @canvas_modules, @canvas_module_items.first.identifier
+      )
+
+      assert_equal result.identifier, @module_ids.first
     end
   end
 
