d908656a1d4ed2556edd5d9965e1a10ae0077515
James Carbine <JamesCarbine@gmail.com>
2017-01-23 14:33:37 -0700

    Add methods for Canvas to call [#136584223] (#56)
    This adds methods that canvas can call to do the converting.
    Changes config so it can be configurable by more than the yml.
    Add configuration specs

--- spec/senkyoshi_spec.rb
+++ spec/senkyoshi_spec.rb
@@ -12,4 +12,49 @@ describe Senkyoshi do
       assert_match /Couldn't find file/, err.message
     end
   end
+
+  describe ".reset" do
+    it "resets the configuration" do
+      Configuration.stub(:_config, {}) do
+        Senkyoshi.configure do |config|
+          config.canvas_url = "bfcoder.com"
+          config.canvas_token = "token"
+          config.account_id = 42
+          config.scorm_url = "example.com"
+          config.scorm_launch_url = "example.com/launch"
+          config.scorm_shared_auth = "12345"
+        end
+        Senkyoshi.reset
+        config = Senkyoshi.configuration
+        assert_nil config.canvas_url
+        assert_nil config.canvas_token
+        assert_equal config.account_id, :self
+        assert_nil config.scorm_url
+        assert_nil config.scorm_launch_url
+        assert_nil config.scorm_shared_auth
+      end
+    end
+  end
+
+  describe ".configure" do
+    it "sets the configuration" do
+      Configuration.stub(:_config, {}) do
+        Senkyoshi.configure do |config|
+          config.canvas_url = "bfcoder.com"
+          config.canvas_token = "token"
+          config.account_id = 42
+          config.scorm_url = "example.com"
+          config.scorm_launch_url = "example.com/launch"
+          config.scorm_shared_auth = "12345"
+        end
+        config = Senkyoshi.configuration
+        assert_equal config.canvas_url, "bfcoder.com"
+        assert_equal config.canvas_token, "token"
+        assert_equal config.account_id, 42
+        assert_equal config.scorm_url, "example.com"
+        assert_equal config.scorm_launch_url, "example.com/launch"
+        assert_equal config.scorm_shared_auth, "12345"
+      end
+    end
+  end
 end
